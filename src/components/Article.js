50ce137e361b868f4944ddc14daf71f6a6abbd50
ngyl88 <ng_yl88@hotmail.com>
2018-06-18 17:15:06 +0800

    centralize voting function, remove yarn.lock

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,21 +3,25 @@ import Vote from "./Vote";
 
 class Article extends Component {
   constructor(props) {
-      super();
-      this.title = props.item.title;
-      this.url = props.item.url;
-      this.author = props.item.author;
-      this.publishedAt = props.item.publishedAt;
-      this.state = {
-          description: props.item.description,
-          votes: props.item.votes
-      }
+    super();
+    this.title = props.item.title;
+    this.url = props.item.url;
+    this.author = props.item.author;
+    this.publishedAt = props.item.publishedAt;
+    this.state = {
+      description: props.item.description,
+      votes: props.item.votes
+    };
   }
 
   render() {
     return (
       <div className="article">
-        <Vote voteCount={this.state.votes} upVote={this.handleUpVote} downVote={this.handleDownVote} />
+        <Vote
+          voteCount={this.state.votes}
+          upVote={() => this.handleVoting(1)}
+          downVote={() => this.handleVoting(-1)}
+        />
         <a href={this.url}>{this.title}</a>
         <p>
           <span className="author">
@@ -31,20 +35,11 @@ class Article extends Component {
     );
   }
 
-  handleVoteChange(newVoteCount) {
+  handleVoting(voteChange) {
     this.setState({
-        votes: newVoteCount
-    })
+      votes: this.state.votes + voteChange
+    });
   }
-
-  handleUpVote = () => {
-      this.handleVoteChange(this.state.votes + 1);
-  }
-
-  handleDownVote = () => {
-      this.handleVoteChange(this.state.votes - 1);
-  }
-
 }
 
 export default Article;
